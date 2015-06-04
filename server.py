431b02f95558f40775b39d84de4b556eef6a1a75
Koji Tsutsumi <koji@tsutsumi.io>
2015-06-03 22:13:12 -0700

    added a way for others to creat cards on my computer

--- server.py
+++ server.py
@@ -2,13 +2,31 @@ from flask import Flask
 
 app = Flask (__name__)
 
+def add_card_to_file(card):
+
+  f = open("card_database.txt", "a+")
+  f.write(card.name + " ," + card.description + "\n")
+  f.close()
+
+def read_cards_from_file():
+  f = open("card_database.txt", "r")
+  cards = f.read()
+  f.close()
+  return cards
+
+
 class Card(object):
 
   def __init__(self, name, description):
     self.name = name
     self.description = description
+    self.tapped = False
 
-cards = []
+  def tap(self):
+    self.tapped = True
+
+  def is_tapped(self):
+    return self.tapped
 
 @app.route("/")
 def hello():
@@ -18,15 +36,13 @@ def hello():
 def add_card(name, description):
   card = Card(name, description)
   cards.append(card)
+  add_card_to_file(card)
   return "your card " + name + " has been added"
 
 @app.route("/get_cards")
 def get_cards():
-  return_string = ""
-  for card in cards:
-    return_string += "(" + card.name + "," + card.description + ")"
-  return return_string
-
+  return read_cards_from_file()
 
 if __name__ == "__main__":
-  app.run()
+
+  app.run(host="0.0.0.0")
