460a3c25cbbf7489e89e409b57e1521dbb3f455c
Koji Tsutsumi <koji@tsutsumi.io>
2015-06-10 23:27:54 -0700

    I added a template to fancy up the joint

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+
 
 app = Flask (__name__)
 
@@ -30,12 +33,15 @@ class Card(object):
 
 @app.route("/")
 def hello():
-  return "Hello, world!"
+  return render_template('index.html', name='Koji')
 
-@app.route("/add_card/<name>/<description>")
-def add_card(name, description):
+
+@app.route("/add_card")
+def add_card():
+  name = request.args.get ("name")
+  description = request.args.get ("description")
   card = Card(name, description)
-  cards.append(card)
+
   add_card_to_file(card)
   return "your card " + name + " has been added"
 
@@ -44,5 +50,5 @@ def get_cards():
   return read_cards_from_file()
 
 if __name__ == "__main__":
-
+  app.debug = True
   app.run(host="0.0.0.0")
